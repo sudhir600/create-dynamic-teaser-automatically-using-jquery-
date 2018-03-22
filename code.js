24e41881ff19e1e8d4bf38720984ba0567caf169
Sudhir Gupta <sudhirgupta.456@gmail.com>
2018-03-22 12:27:19 +0530

    Live Demo HTML added

--- code.js
+++ code.js
@@ -2,38 +2,49 @@
   Author - Sudhir gupta (sudhirgupta.456@gmail.com) 
   this function will trim your text into teaser and add show more button 
   */
-
-function makeTeaser(target,teaseLimit) {
+  var labelMore = 'Show More...';
+  var labelLess = 'Show Less...';
+  var teaserLimit = 5;
   
-  var strings = target.text();
-  var totalChar = strings.length;
-  var teaserText = strings.substr(0, teaseLimit);
-  var restText = strings.substr(teaseLimit, totalChar);
-
-
-  if (totalChar > teaseLimit) {
-    target.html('<span class="teaser">' + teaserText + '+</span><span class="teaseRestText" style="display:none">' + restText + '</span><span class="teaserDot" toggle="0"><i style="color:red">&nbsp; Show more...&nbsp;</i></span>');
-    }
+  function makeTeaser(target,teaseLimit) {
+	  var strings = target.text().trim();
+	  var totalChar = strings.length;
+	  var teaserText = strings.substr(0, teaseLimit);
+	  var restText = strings.substr(teaseLimit, totalChar);
+	  if (totalChar > teaseLimit) {
+		target.html('<span class="teaser">' + teaserText + '</span>\
+					<span class="teaseRestText" style="display:none">' + teaserText + restText + '</span>\
+					<span class="teaserDot" toggle="0">\
+						<i style="color:red">&nbsp; '+ labelMore + '&nbsp;</i>\
+					</span>');
+		}
 }
 
-/* toggle of show more and show less */
 
-$('body').on('click','.teaserDot',function () {
+/* toggle of show more and show less */
+$(document).delegate('.teaserDot', 'click', function () {
     $(this).prev('.teaseRestText').toggle();
+    $(this).prevAll('.teaser').toggle();
+	
     if ($(this).attr('toggle') == '0') {
-        $(this).html('Show Less...').attr('toggle', 1);
+        $(this).html(labelLess).attr('toggle', 1);
     }
     else {
-        $(this).html('Show More...').attr('toggle', 0);
+        $(this).html(labelMore).attr('toggle', 0);
     }
 });
 
-
-/* uses */
-
-$(window).load(function () {
-    $('#tableId tbody td').each(function (e) {
-      target=$(this);
-      makeTeaser(target,40); //40 is a charactor limit.
-    });
-});
+/* Uses- call this function in HTML File */
+function applyTeaser(targetObject){
+	$(window).load(function () {
+		targetObject.each(function (e) {
+		  var target = $(this);
+		  var teaserLimit = target.attr('teaserLimit');
+		  if(typeof teaserLimit == "undefined" || teaserLimit == 0 || isNaN(parseFloat(teaserLimit))) {
+			teaserLimit = 5
+		  }
+		  console.log(teaserLimit);
+		  makeTeaser(target,teaserLimit);
+		});
+	});
+}
